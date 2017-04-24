c10e3c1bf437152d1f5680ed0235f95d62712e7b
张志超 <zzc1026503117zzc@gmail.com>
2017-04-24 13:16:07 +0800

    配置项

---
+++ src/KLine.js
@@ -0,0 +1,50 @@
+import setData from './setData';
+export function KLine(ele, option) {
+    if (option === undefined || option === null) {
+        option = {};
+    }
+    this.ele = ele;
+    this.setOption(option);
+}
+
+KLine.prototype = {
+    setOption,
+    setData,
+};
+
+function setOption(option) {
+    if (option.width === undefined || option.widh === null) {
+        console.error('option.width must be number');
+    }
+    if (option.height === undefined || option.height === null) {
+        console.error('option.height must be number');
+    }
+    // 配置项
+    this.option = {
+        theme: option.theme || 'dark',
+        width: option.width,
+        height: option.height,
+        intervalY: option.intervalY || 200,
+        intervalX: option.intervalX || 50,
+        maxKLineNumber: option.maxKLineNumber || parseInt(option.width / 2),
+        minKLineNumber: option.minKLineNumber || 20,
+        yAxisWidth: option.yAxisWidth || 120,
+        fontSize: option.fontSize || 14,
+        csi: option.csi || 'ema',
+        csi2: option.csi2 || ['macd'],
+        onChange: option.onChange || function() {},
+        onSelect: option.onSelect || this.select,
+        timeFilter: option.timeFilter || (t => {
+            return new Date(t * 1000).toLocaleDateString();
+        }),
+        priceFilter: option.priceFilter || (d => {
+            return Number(d.toFixed(2));
+        }),
+        overTimeFilter: option.overTimeFilter || (t => {
+            return new Date(t * 1000).toLocaleTimeString();
+        }),
+        overYFilter: option.overYFilter || (d => {
+            return Number(d.toFixed(2));
+        }),
+    };
+}
