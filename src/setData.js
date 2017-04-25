5404c19ce5bf8baa9bd607a34879c906603583d8
张志超 <zzc1026503117zzc@gmail.com>
2017-04-25 17:25:57 +0800

    优化setData方法

--- src/setData.js
+++ src/setData.js
@@ -1,55 +1,69 @@
 export default function setData(data) {
+    let times = [];
+    let start = [];
+    let hi = [];
+    let lo = [];
+    let close = [];
+    let volume = [];
+    data.forEach(d => {
+        times.push(d[0]);
+        start.push(d[1]);
+        hi.push(d[2]);
+        lo.push(d[3]);
+        close.push(d[4]);
+        volume.push(d[5]);
+    });
     this.state = {
         startIndex: data.length - 50,
         endIndex: data.length,
         verticalRectNumber: 50,
         isDown: false,
-        times: data.map(d => d[0]),
-        start: data.map(d => d[1]),
-        hi: data.map(d => d[2]),
-        lo: data.map(d => d[3]),
-        close: data.map(d => d[4]),
-        volume: data.map(d => d[5]),
-        ma30: (data.map(d => d[4])).map((el, i) => {
+        times,
+        start,
+        hi,
+        lo,
+        close,
+        volume,
+        ma30: close.map((el, i) => {
             if (i < 29) {
                 return el;
             } else {
                 let sum = 0;
                 for (let index = i; index > i - 30; index--) {
-                    sum += data.map(d => d[4])[index];
+                    sum += close[index];
                 }
                 return this.setDP(sum / 30);
             }
         }),
-        ma7: (data.map(d => d[4])).map((el, i) => {
+        ma7: close.map((el, i) => {
             if (i < 6) {
                 return el;
             } else {
                 let sum = 0;
                 for (let index = i; index > i - 7; index--) {
-                    sum += data.map(d => d[4])[index];
+                    sum += close[index];
                 }
                 return this.setDP(sum / 7);
             }
         }),
-        volumeMa7: (data.map(d => d[5])).map((el, i) => {
+        volumeMa7: volume.map((el, i) => {
             if (i < 6) {
                 return el;
             } else {
                 let sum = 0;
                 for (let index = i; index > i - 7; index--) {
-                    sum += data.map(d => d[5])[index];
+                    sum += volume[index];
                 }
                 return this.setDP(sum / 7);
             }
         }),
-        volumeMa30: (data.map(d => d[5])).map((el, i) => {
+        volumeMa30: volume.map((el, i) => {
             if (i < 29) {
                 return el;
             } else {
                 let sum = 0;
                 for (let index = i; index > i - 30; index--) {
-                    sum += data.map(d => d[5])[index];
+                    sum += volume[index];
                 }
                 return this.setDP(sum / 30);
             }
