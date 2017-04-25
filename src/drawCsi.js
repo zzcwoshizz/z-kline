4b35ff9a2bb6ee5d23690bf73bfe26c2392a660b
张志超 <zzc1026503117zzc@gmail.com>
2017-04-25 18:52:16 +0800

    优化drawCsi

--- src/drawCsi.js
+++ src/drawCsi.js
@@ -134,28 +134,47 @@ function drawMacd(view1, view2) {
 
     ctx.setLineDash([]);
     ctx.lineWidth = this.dpr;
+    ctx.fillStyle = this.colors.greenColor;
+    ctx.strokeStyle = this.colors.greenColor;
     for (let i = this.state.startIndex, j = 0; i < this.state.endIndex; i++, j++) {
         if (i >= this.state.times.length) {
             break;
         }
         if (this.state.macd[i] > 0) {
-            ctx.fillStyle = this.colors.greenColor;
-            ctx.strokeStyle = this.colors.greenColor;
-        } else {
-            ctx.fillStyle = this.colors.redColor;
-            ctx.strokeStyle = this.colors.redColor;
-        }
-        let y = view1.y + view1.h * 0.5;
-        let w = view1.w / this.state.verticalRectNumber * 0.8;
-        let x = j * view1.w / this.state.verticalRectNumber + view1.x + w * 0.1;
-        let h = -this.state.macd[i] / max * view1.h * 0.5;
-        if (Math.abs(this.state.macd[i]) > Math.abs(this.state.macd[i - 1])) {
-            ctx.fillRect(x, y, w, h);
-        } else {
-            if (w <= this.dpr * 4) {
+            let y = view1.y + view1.h * 0.5;
+            let w = view1.w / this.state.verticalRectNumber * 0.8;
+            let x = j * view1.w / this.state.verticalRectNumber + view1.x + w * 0.1;
+            let h = -this.state.macd[i] / max * view1.h * 0.5;
+            if (Math.abs(this.state.macd[i]) > Math.abs(this.state.macd[i - 1])) {
                 ctx.fillRect(x, y, w, h);
             } else {
-                ctx.strokeRect(x, y, w, h);
+                if (w <= this.dpr * 4) {
+                    ctx.fillRect(x, y, w, h);
+                } else {
+                    ctx.strokeRect(x, y, w, h);
+                }
+            }
+        }
+    }
+    ctx.fillStyle = this.colors.redColor;
+    ctx.strokeStyle = this.colors.redColor;
+    for (let i = this.state.startIndex, j = 0; i < this.state.endIndex; i++, j++) {
+        if (i >= this.state.times.length) {
+            break;
+        }
+        if (this.state.macd[i] <= 0) {
+            let y = view1.y + view1.h * 0.5;
+            let w = view1.w / this.state.verticalRectNumber * 0.8;
+            let x = j * view1.w / this.state.verticalRectNumber + view1.x + w * 0.1;
+            let h = -this.state.macd[i] / max * view1.h * 0.5;
+            if (Math.abs(this.state.macd[i]) > Math.abs(this.state.macd[i - 1])) {
+                ctx.fillRect(x, y, w, h);
+            } else {
+                if (w <= this.dpr * 4) {
+                    ctx.fillRect(x, y, w, h);
+                } else {
+                    ctx.strokeRect(x, y, w, h);
+                }
             }
         }
     }
