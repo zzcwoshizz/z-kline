61ca9323207445b795d1de955fdd013210dde67f
张志超 <zzc1026503117zzc@gmail.com>
2017-05-16 11:06:28 +0800

    split line

--- src2/draw.js
+++ src2/draw.js
@@ -4,6 +4,7 @@ export default function draw() {
 
     drawBackground.call(this);
     drawTime.call(this);
+    drawSplitLine.call(this);
 
     const yaxis = this.computAxis();
 
@@ -22,4 +23,18 @@ function drawTime() {
     const ctx = this.ctx;
     ctx.fillStyle = this.colors.timeBackground;
     ctx.fillRect(0, this.timeView.y, this.width, this.timeView.h);
+}
+
+function drawSplitLine() {
+    const ctx = this.ctx;
+    ctx.strokeStyle = this.colors.splitLine;
+    ctx.beginPath();
+    ctx.moveTo(0, (this.mainView.h + this.mainView.y + this.aidView.y) * 0.5);
+    ctx.lineTo(this.width, (this.mainView.h + this.mainView.y + this.aidView.y) * 0.5);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(this.mainYaxisView.x, 0);
+    ctx.lineTo(this.aidYaxisView.x, this.aidYaxisView.y + this.aidYaxisView.h);
+    ctx.stroke();
 }
